471add35a43be36a873b9a1f613878dfea55eed7
Marcial Rosales <mrosales@pivotal.io>
2019-07-19 19:49:52 +0200

    Organize imports

--- reactive-amqp-importer/src/main/java/com/pivotal/rabbitmq/importer/SpringBootSample.java
+++ reactive-amqp-importer/src/main/java/com/pivotal/rabbitmq/importer/SpringBootSample.java
@@ -16,15 +16,8 @@
 
 package com.pivotal.rabbitmq.importer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import reactor.rabbitmq.Receiver;
-import reactor.rabbitmq.Sender;
 
 @SpringBootApplication
 public class SpringBootSample {
